42bbe9148bece732369d64ef8c9b9553d57d2c2f
mctminhtai <mct.minhtai@gmail.com>
2021-03-23 10:54:16 +0700

    sua loi

--- controllers/webhookCtrl.js
+++ controllers/webhookCtrl.js
@@ -1,44 +1,91 @@
 const request = require('request');
 exports.post_webhook = function (req, res, next) {
     let body = req.body;
+
+    // Checks this is an event from a page subscription
     if (body.object === 'page') {
+
+        // Iterates over each entry - there may be multiple if batched
         body.entry.forEach(function (entry) {
+
+            // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
+            //console.log(webhook_event);
+
+            // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
             if (webhook_event.message) {
                 handleMessage(sender_psid, webhook_event.message);
             } else if (webhook_event.postback) {
                 handlePostback(sender_psid, webhook_event.postback);
             }
+
         });
+
+        // Returns a '200 OK' response to all requests
         res.status(200).send('EVENT_RECEIVED');
     } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
     }
 }
 exports.get_webhook = function (req, res, next) {
     let VERIFY_TOKEN = "minhtai"
+
+    // Parse the query params
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
     if (mode && token) {
+
+        // Checks the mode and token sent is correct
         if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Responds with the challenge token from the request
             console.log('WEBHOOK_VERIFIED');
             res.status(200).send(challenge);
+
         } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
             res.sendStatus(403);
         }
     }
 }
 const PAGE_ACCESS_TOKEN = "EAASGhGZBXOZCABADJDr1qPE26Yh2JXHzfYeS1H8tPXc64g5TZBV2hgoEitqUZBc0ZA3ztgQRX670Rw1fKZBxN23NfqTV1zOTZA3RntZCOmubkZClQxqdqkMEntfMjW5bWV6safhxbA7IdqlwovyOKn1ZAyKdIDY8A7QdAec3sFdZA0TN0d8NA1sv3C559OJdPZAeQdEZD"
+
+
+// Handles messages events
 function handleMessage(sender_psid, received_message) {
     let response;
+
+    // Check if the message contains text
     if (received_message.text) {
+
+        // Create the payload for a basic text message
         response = {
+            //"text": `You sent the message: "${received_message.text}". Now send me an image!`
             "text": "Cảm ơn bạn đã nhắn tin đến cho WAKAME, chúng mình sẽ phản hồi sớm nhất có thể."
+            // "text": "Pick a color:",
+            // "quick_replies": [
+            //     {
+            //         "content_type": "text",
+            //         "title": "Red",
+            //         "payload": "<POSTBACK_PAYLOAD>"
+            //     }, {
+            //         "content_type": "text",
+            //         "title": "Green",
+            //         "payload": "<POSTBACK_PAYLOAD>"
+            //     }
+            // ]
         }
     } else if (received_message.attachments) {
+
+        // Gets the URL of the message attachment
         let attachment_url = received_message.attachments[0].payload.url;
         response = {
             "attachment": {
@@ -66,28 +113,41 @@ function handleMessage(sender_psid, received_message) {
             }
         }
     }
+
+    // Sends the response message
     callSendAPI(sender_psid, response);
 }
 
+// Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
     let response;
+
+    // Get the payload for the postback
     let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
     if (payload === 'yes') {
         response = { "text": "Thanks!" }
     } else if (payload === 'no') {
         response = { "text": "Oops, try sending another image." }
     }
+    // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
 }
 
+// Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
+    // Construct the message body
     let request_body = {
         "recipient": {
             "id": sender_psid
         },
         "message": response
     }
+
+    // Send the HTTP request to the Messenger Platform
     request({
+        "uri": "https://graph.facebook.com/v10.0/me/messages",
         "qs": { "access_token": PAGE_ACCESS_TOKEN },
         "method": "POST",
         "json": request_body
