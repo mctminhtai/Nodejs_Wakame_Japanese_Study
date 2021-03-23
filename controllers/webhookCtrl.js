67154706e169ef570fbbafcf8b168b51771a7927
mctminhtai <mct.minhtai@gmail.com>
2021-03-23 11:00:09 +0700

    xoa mot nua comment con lai

--- controllers/webhookCtrl.js
+++ controllers/webhookCtrl.js
@@ -84,34 +84,24 @@ function handleMessage(sender_psid, received_message) {
     callSendAPI(sender_psid, response);
 }
 
-// Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
     let response;
-
-    // Get the payload for the postback
     let payload = received_postback.payload;
-
-    // Set the response based on the postback payload
     if (payload === 'yes') {
         response = { "text": "Thanks!" }
     } else if (payload === 'no') {
         response = { "text": "Oops, try sending another image." }
     }
-    // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
 }
 
-// Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-    // Construct the message body
     let request_body = {
         "recipient": {
             "id": sender_psid
         },
         "message": response
     }
-
-    // Send the HTTP request to the Messenger Platform
     request({
         "uri": "https://graph.facebook.com/v10.0/me/messages",
         "qs": { "access_token": PAGE_ACCESS_TOKEN },
