d58930e02c18524f0a0354280adcb7153d429b20
mctminhtai <mct.minhtai@gmail.com>
2021-03-24 01:33:00 +0700

    thay doi response

--- controllers/webhookCtrl.js
+++ controllers/webhookCtrl.js
@@ -77,29 +77,7 @@ function handleMessage(sender_psid, received_message) {
     } else if (received_message.attachments) {
         let attachment_url = received_message.attachments[0].payload.url;
         response = {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [{
-                        "title": "Is this the right picture?",
-                        "subtitle": "Tap a button to answer.",
-                        "image_url": attachment_url,
-                        "buttons": [
-                            {
-                                "type": "postback",
-                                "title": "Yes!",
-                                "payload": "yes",
-                            },
-                            {
-                                "type": "postback",
-                                "title": "No!",
-                                "payload": "no",
-                            }
-                        ],
-                    }]
-                }
-            }
+            "attachment": received_message.attachments
         }
     }
     callSendAPI(sender_psid, response);
