d8a5b59f8c93affca2256a2d8380e3ac8704387f
mctminhtai <mct.minhtai@gmail.com>
2021-03-31 21:00:25 +0700

    Revert "fdsf"
    This reverts commit b099cb64af978e4ade8df7b85073289ae3beb27f.

--- controllers/newsPage.js
+++
@@ -1,3 +0,0 @@
-exports.get_newsPage = function (req, res, next) {
-    res.render('news', { title: 'Express' });
-}
--- routes/newsRoute.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var router = express.Router();
-let newsPage = require('../controllers/newsPage');
-/* GET home page. */
-router.get('/', newsPage.get_newsPage);
-module.exports = router;
