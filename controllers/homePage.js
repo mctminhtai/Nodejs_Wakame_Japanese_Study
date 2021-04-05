0387c655a06f4f32fe66cceea9e9971249df3243
Vietanh239 <72076528+Vietanh239@users.noreply.github.com>
2021-04-05 23:18:44 +0700

    Tạo layout
     các file pug kế thừa layout

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,18 +1,42 @@
 exports.get_homePage = function (req, res, next) {
-    return res.render('index', { title: 'Express' });
+    var xacnhan=false;
+    if (req.isAuthenticated()) {
+        xacnhan=true;
+    }
+    return res.render('index', { title: 'Express',Authenticated:xacnhan });
 }
 exports.get_aboutPage = function (req, res, next) {
-    return res.render('about', { title: 'Express' });
+    var xacnhan=false;
+    if (req.isAuthenticated()) {
+        xacnhan=true;
+    }
+    return res.render('about', { title: 'Express',Authenticated:xacnhan });
 }
 exports.get_blogPage = function (req, res, next) {
-    return res.render('blog', { title: 'Express' });
+    var xacnhan=false;
+    if (req.isAuthenticated()) {
+        xacnhan=true;
+    }
+    return res.render('blog', { title: 'Express',Authenticated:xacnhan });
 }
 exports.get_blogDetailPage = function (req, res, next) {
-    return res.render('blog_details', { title: 'Express' });
+    var xacnhan=false;
+    if (req.isAuthenticated()) {
+        xacnhan=true;
+    }
+    return res.render('blog_details', { title: 'Express',Authenticated:xacnhan });
 }
 exports.get_contactPage = function (req, res, next) {
-    return res.render('contact', { title: 'Express' });
+    var xacnhan=false;
+    if (req.isAuthenticated()) {
+        xacnhan=true;
+    }
+    return res.render('contact', { title: 'Express' ,Authenticated:xacnhan});
 }
 exports.get_coursesPage = function (req, res, next) {
-    return res.render('courses', { title: 'Express' });
+    var xacnhan=false;
+    if (req.isAuthenticated()) {
+        xacnhan=true;
+    }
+    return res.render('courses', { title: 'Express',Authenticated:xacnhan });
 }
