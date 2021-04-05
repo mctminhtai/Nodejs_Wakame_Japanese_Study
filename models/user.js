914ab7f0f779850a0b1b99abd1bc0a2eff82555c
thientam332 <76957177+thientam332@users.noreply.github.com>
2021-04-05 12:44:42 +0700

    Test ty
    Hi ALL

--- models/user.js
+++ models/user.js
@@ -1,28 +1,29 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class USER extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      this.hasMany(models.BLOG, { as: 'baidang' });
-      this.hasMany(models.COMMENT, { as: 'binhluan' });
-    }
-  };
-  USER.init({
-    fullName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'USER',
-    freezeTableName: true,
-  });
-  return USER;
+    class USER extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            this.hasMany(models.BLOG, { as: 'baidang' });
+            this.hasMany(models.COMMENT, { as: 'binhluan' });
+        }
+    };
+    USER.init({
+        fullName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING
+    }, {
+            sequelize,
+            modelName: 'USER',
+            freezeTableName: true,
+        });
+    return USER;
+    //No god please!!!
 };
