25052aa3851488626470e5f075166178231b158a
Vietanh239 <72076528+Vietanh239@users.noreply.github.com>
2021-04-06 10:11:55 +0700

    chỉnh userme

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,42 +1,66 @@
+
 exports.get_homePage = function (req, res, next) {
     var xacnhan=false;
+
     if (req.isAuthenticated()) {
         xacnhan=true;
+        return res.render('index', { title: 'Express',Authenticated:xacnhan,user_name:req.user.dataValues.fullName});
     }
-    return res.render('index', { title: 'Express',Authenticated:xacnhan });
+    else{
+        return res.render('index', { title: 'Express',Authenticated:xacnhan});
+    }
+   
 }
 exports.get_aboutPage = function (req, res, next) {
     var xacnhan=false;
     if (req.isAuthenticated()) {
         xacnhan=true;
+        return res.render('about', { title: 'Express',Authenticated:xacnhan ,user_name:req.user.dataValues.fullName});
     }
-    return res.render('about', { title: 'Express',Authenticated:xacnhan });
+    else{
+        return res.render('about', { title: 'Express',Authenticated:xacnhan,user_name:req.user.dataValues.fullName });
+    }
+
 }
 exports.get_blogPage = function (req, res, next) {
     var xacnhan=false;
     if (req.isAuthenticated()) {
         xacnhan=true;
+        return res.render('about', { title: 'Express',Authenticated:xacnhan,user_name:req.user.dataValues.fullName });
     }
+    else{
     return res.render('blog', { title: 'Express',Authenticated:xacnhan });
+}
 }
 exports.get_blogDetailPage = function (req, res, next) {
     var xacnhan=false;
     if (req.isAuthenticated()) {
         xacnhan=true;
+        return res.render('about', { title: 'Express',Authenticated:xacnhan,user_name:req.user.dataValues.fullName });
     }
-    return res.render('blog_details', { title: 'Express',Authenticated:xacnhan });
+    else{
+        return res.render('blog_details', { title: 'Express',Authenticated:xacnhan });
+    }
+
 }
 exports.get_contactPage = function (req, res, next) {
     var xacnhan=false;
     if (req.isAuthenticated()) {
         xacnhan=true;
+        return res.render('about', { title: 'Express',Authenticated:xacnhan,user_name:req.user.dataValues.fullName });
     }
-    return res.render('contact', { title: 'Express' ,Authenticated:xacnhan});
+    else{
+     return res.render('contact', { title: 'Express' ,Authenticated:xacnhan});
+    }
 }
 exports.get_coursesPage = function (req, res, next) {
     var xacnhan=false;
     if (req.isAuthenticated()) {
         xacnhan=true;
+        return res.render('about', { title: 'Express',Authenticated:xacnhan,user_name:req.user.dataValues.fullName });
     }
-    return res.render('courses', { title: 'Express',Authenticated:xacnhan });
+    else{
+        return res.render('courses', { title: 'Express',Authenticated:xacnhan });
+    }
+    
 }
