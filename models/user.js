d36764dfcd36be8bc2a1187f38ee4619c483704a
mctminhtai <mct.minhtai@gmail.com>
2021-04-13 21:35:47 +0700

    sua loi

--- models/user.js
+++ models/user.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
             // define association here
             this.hasMany(models.BLOG, { as: 'user_blog' });
             this.hasMany(models.COMMENT, { as: 'user_comment' });
-            this.belongsToMany(models.BLOG,)
+
             this.hasMany(models.TKB_DU_KIEN, { as: 'user_tkb' });
             this.belongsToMany(models.MONHOC, { through: models.DS_MON_DA_HOC, foreignKey: 'USERId', as: 'user_monhoc' });
         }
