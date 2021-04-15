a2b6f55fa047a0cee3edac14609fbed5d108d071
mctminhtai <mct.minhtai@gmail.com>
2021-04-15 21:22:43 +0700

    them tinh nang reset mat khau qua email

--- controllers/loginRegisterPage.js
+++ controllers/loginRegisterPage.js
@@ -3,6 +3,8 @@ var svgCaptcha = require('svg-captcha');
 const bcrypt = require('bcryptjs');
 var passport = require('passport');
 const { v4: uuidv4 } = require('uuid');
+const randostring = require('randostrings/server');
+let randomString = new randostring();
 //add ham gui email
 var mailer = require('../utils/mailer');
 
@@ -50,6 +52,7 @@ passport.use('local.signup', new LocalStrategy({
                     email: req.body.email,
                     password: hash
                 }).then(user => {
+                    //console.log(user.email, req.body.email);
                     return done(null, user);
                 }).catch(error => {
                     console.log(error);
@@ -77,15 +80,62 @@ exports.get_logout = function (req, res, next) {
     req.logout();
     return res.redirect('/');
 }
-exports.get_resetPwd = function (req, res, next) {
-    res.render('pwdReset_login');
+exports.get_resetToken = function (req, res, next) {
+    res.render('sendToken_login');
 }
-exports.post_resetPwd = function (req, res, next) {
-    console.log(req.body);
-    res.render('pwdReset_login');
+exports.post_resetToken = function (req, res, next) {
+    var randString = randomString.password({
+        length: 100,
+        string: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    });
+    var resetPwdUrl = req.headers.origin + '/pwd_reset' + '/' + randString;
+    console.log(resetPwdUrl);
+    mailer.sendMail(req.body.email, 'test thử email CLB', resetPwdUrl);
+    models.RESETTOKEN.create({
+        token: randString,
+        email: req.body.email,
+    });
+    res.render('sendToken_login');
 }
-
-
+exports.get_resetPwd = async function (req, res, next) {
+    var received_token = req.param('token');
+    savedToken = await models.RESETTOKEN.findOne({
+        where: { token: received_token }
+    });
+    if (savedToken) {
+        res.render('resetPwd_login', {
+            token: savedToken.token,
+            email: savedToken.email,
+        });
+    } else {
+        res.send('token khong dung');
+    }
+}
+exports.post_resetPwd = async function (req, res, next) {
+    var received_token = req.param('token');
+    savedToken = await models.RESETTOKEN.findOne({
+        where: { token: received_token }
+    });
+    if (savedToken) {
+        var hashPwd = bcrypt.hashSync(req.body.password, 10);
+        models.USER.update(
+            { password: hashPwd },
+            {
+                where: {
+                    email: req.body.email,
+                }
+            }
+        );
+        models.RESETTOKEN.destroy({
+            where: {
+                email: req.body.email,
+            }
+        });
+        res.redirect('/');
+    } else {
+        res.send('token khong dung');
+    }
+}
 exports.get_captcha = function (req, res, next) {
     var captcha = svgCaptcha.create();
     //console.log(captcha);
--- routes/loginRegister.js
+++ routes/loginRegister.js
@@ -9,7 +9,7 @@ const { body } = require('express-validator');
 router.get('/accounts', checkAuth.checkNotAuthenticated, loginRegister.get_accountsPage);
 router.post(
     '/login',
-    body('email', 'Email không đúng định dạng').isEmail().normalizeEmail(),
+    body('email', 'Email không đúng định dạng').isEmail(),
     validate.checkErr,
     passport.authenticate(
         'local.login',
@@ -18,7 +18,7 @@ router.post(
 router.post(
     '/register',
     body('name', 'tên phải lớn hơn 5 ký tự').isLength({ min: 5 }),
-    body('email', 'email chưa đúng định dạng').isEmail().normalizeEmail(),
+    body('email', 'email chưa đúng định dạng').isEmail(),
     body('password', 'password phải từ 8 ký tự trở lên').isLength({ min: 8 }),
     body('re_password').custom((value, { req }) => {
         if (value !== req.body.password) {
@@ -33,8 +33,10 @@ router.post(
     )
 );
 router.get('/logout', loginRegister.get_logout);
-router.get('/pwd_reset', loginRegister.get_resetPwd);
-router.post('/pwd_reset', loginRegister.post_resetPwd);
+router.get('/pwd_reset', loginRegister.get_resetToken);
+router.post('/pwd_reset', loginRegister.post_resetToken);
+router.get('/pwd_reset/:token', loginRegister.get_resetPwd);
+router.post('/pwd_reset/:token', loginRegister.post_resetPwd);
 router.get('/test', loginRegister.get_test);
 router.get('/captcha', loginRegister.get_captcha);
 router.post('/captcha', loginRegister.post_captcha);
