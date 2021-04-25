9f6c71f61d092c7d0b682c68c838542167285be7
NamNguyenPN <abc2341999@gmail.com>
2021-04-25 11:37:39 +0700

    add form passcode

--- public/assets/js/account.js
+++ public/assets/js/account.js
@@ -1,6 +1,9 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
+window.onload=function showForm(){
+  document.getElementById("form").style.opacity=1;
+}
 
 sign_up_btn.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
@@ -13,4 +16,6 @@ function offNoti() {
   element = document.getElementById("error");
   element.classList.remove("flag");
 }
+
+
 
