9c4a2c793f4158a151df8ad355e333c6a96a1cb8
mctminhtai <mct.minhtai@gmail.com>
2021-05-25 23:49:56 +0700

    tu dong thay doi meta tag description cho tung bai viet

--- controllers/adminCtrl.js
+++ controllers/adminCtrl.js
@@ -5,6 +5,12 @@ exports.getAdminPage = function (req, res, next) {
 exports.getAdminManagBlog = function (req, res, next) {
     res.render('admin/manage_blog');
 }
-exports.getAdminAddBlog = function (req, res, next) {
-    res.render('admin/add_blog');
+exports.getAdminAddBlog = async function (req, res, next) {
+    categories = await models.CATEGORY.findAll();
+    results = await Promise.all([
+        models.CATEGORY.findAll(),
+        models.TAG.findAll()
+    ]);
+    console.log(results[0][0].constructor.name);
+    res.render('admin/add_blog', { categories: categories });
 }
