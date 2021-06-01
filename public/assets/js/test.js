491203a42fcbdc7a248f2e36c7e7a54e9df04819
mctminhtai <mct.minhtai@gmail.com>
2021-06-02 03:03:20 +0700

    thay doi test.js

--- public/assets/js/test.js
+++ public/assets/js/test.js
@@ -1,5 +1,4 @@
 var url = window.location.href;
-console.log(location.origin);
 var id = url.split(`${location.origin}/`)[1].split("/")[0];
 var loc = document.querySelectorAll("a[href='" + '/' + id + "']");
 if (id.length != 0) {
