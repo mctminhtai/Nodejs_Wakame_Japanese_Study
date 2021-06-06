8c8a87c9e23d7456a1f16299241f38e63e89f456
mctminhtai <mct.minhtai@gmail.com>
2021-06-06 13:29:32 +0700

    sua loi giao dien

--- public/assets/js/test.js
+++ public/assets/js/test.js
@@ -1,7 +1,10 @@
 var url = window.location.href;
 var id = url.split(`${location.origin}/`)[1].split("/")[0];
-var loc = document.querySelectorAll("a[href='" + '/' + id + "']");
-if (id.length != 0) {
+var loc = document.querySelectorAll("#navigation .nav-item");
+if (id.length == 0) {
     loc[0].classList.add("onpage");
+} else {
+    loc = document.querySelector("#navigation a[href='/" + id + "']");
+    loc.classList.add("onpage");
 }
 
